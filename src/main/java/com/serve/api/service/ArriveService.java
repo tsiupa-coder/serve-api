a2533fd5f70059b932709c80693fb0128fd6617b
ivan.tsiupa <ivan.tsupa111@gmail.com>
2022-08-01 11:25:57 +0300

    main idea

--- src/main/java/com/serve/api/service/ArriveService.java
+++ src/main/java/com/serve/api/service/ArriveService.java
@@ -3,12 +3,17 @@ package com.serve.api.service;
 import com.serve.api.dto.ArriveDto;
 import com.serve.api.mapper.ArriveMapper;
 import com.serve.api.model.entity.Arrive;
+import com.serve.api.model.enumeration.Type;
 import com.serve.api.repository.ArriveRepository;
+import liquibase.exception.DatabaseException;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.experimental.FieldDefaults;
 import org.springframework.stereotype.Service;
 
+import java.time.Period;
+import java.util.Date;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -50,4 +55,53 @@ public class ArriveService {
         if (Objects.isNull(id)) throw new NullPointerException("Id is null");
         repository.deleteById(id);
     }
+
+    /**
+     *  тут потрібно реалізувати функціонал
+     *
+     *  щоб поверталися години для працівника
+     *
+     *  за певною датою
+     *
+     */
+    public int getHours(Long workerId, Date date){
+
+        return 0;
+    }
+
+
+    public int getHours(Long workerId, Date start, Date end) {
+
+
+        return 0;
+    }
+
+
+    /***
+     * повертаються години
+     *
+     * при чому одного типу (вхід, вихід)
+     *
+     * За певний період
+     */
+    public int getHours(Long userId, Period period, Type type){
+
+
+        return 0;
+    }
+
+    public int getHours(Long workerId, Period period){
+
+        //якщо кількість входів != кількості виходів  == помилка
+        // вихід - вхід = робоічі години
+        // загальні += робочі години
+        // return загальні - кількість входів * робочий день(години)
+
+
+
+        return 0;
+    }
+
+
+
 }
